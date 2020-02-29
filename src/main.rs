cc3c94a2ca256b4365745cfd9fc32590ac80a6f6
mumusan <mxx_ne@163.com>
2020-02-29 09:57:04 +0800

    1

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use minigrep::Config;
+use std::env;
+use std::process;
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    if let Err(e) = minigrep::run(config) {
+        println!("App Error: {}", e);
+        process::exit(1);
+    }
+}
