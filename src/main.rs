408c1a9ea91c43c8bdab3fc43a48451acb4daec3
mumusan <mxx_ne@163.com>
2020-03-08 11:03:53 +0800

    fin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use oop::Post;
 fn main() {
-    println!("Hello, world!");
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
+
+    println!("Ok");
 }
